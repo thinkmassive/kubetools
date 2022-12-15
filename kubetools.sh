f76266e43db2ab9942814a96fdce0b54b5081cab
Miller <alex@thinkmassive.org>
2022-12-15 00:19:44 -0500

    kubetools.sh: add argocd, tektoncd-cli, knative

--- kubetools.sh
+++ kubetools.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-KUBETOOLS_ALIASES="${KUBETOOLS_ALIASES:-kubetools helm kubectl jsonnet jb}"
+KUBETOOLS_ALIASES="${KUBETOOLS_ALIASES:-kubetools helm kubectl jsonnet jb argocd tektoncd-cli kn}"
 KUBETOOLS_DIR_KUBE="${KUBETOOLS_DIR_KUBE:-$HOME/.kube}"
-KUBETOOLS_IMAGE=${KUBETOOLS_IMAGE:-public.ecr.aws/a1r4w4i5/kubetools}
+KUBETOOLS_IMAGE=${KUBETOOLS_IMAGE:-thinkmassive/kubetools}
 KUBETOOLS_LOGLEVEL=${KUBETOOLS_LOGLEVEL:-1}
 KUBETOOLS_TAG=${KUBETOOLS_TAG:-latest}
 
@@ -20,6 +20,9 @@ HELM_CMD="$KUBETOOLS_RUN helm"
 KUBECTL_CMD="$KUBETOOLS_RUN kubectl"
 JSONNET_CMD="$KUBETOOLS_RUN jsonnet"
 JSONNETBUNDLER_CMD="$KUBETOOLS_RUN jb"
+ARGOCD_CMD="$KUBETOOLS_RUN argocd"
+TEKTONCLI_CMD="$KUBETOOLS_RUN tektoncd-cli"
+KNATIVE_CMD="$KUBETOOLS_RUN kn"
 
 for cmd in $KUBETOOLS_ALIASES; do
   [[ $KUBETOOLS_LOGLEVEL -gt 1 ]] && echo -en "\n$cmd:\t"
@@ -27,6 +30,15 @@ for cmd in $KUBETOOLS_ALIASES; do
     kubetools)
       [[ $KUBETOOLS_LOGLEVEL -gt 1 ]] && echo "$KUBETOOLS_RUN /bin/sh"
       alias kubetools="$KUBETOOLS_RUN /bin/sh";;
+    argocd)
+      alias argocd="$ARGOCD_CMD"
+      [[ $KUBETOOLS_LOGLEVEL -gt 1 ]] && argocd version;;
+    tektoncd-cli)
+      alias tektoncd-cli="$TEKTONCLI_CMD"
+      [[ $KUBETOOLS_LOGLEVEL -gt 1 ]] && tektoncd-cli version;;
+    knative)
+      alias kn="$KNATIVE_CMD"
+      [[ $KUBETOOLS_LOGLEVEL -gt 1 ]] && kn version;;
     helm)
       alias helm="$HELM_CMD"
       [[ $KUBETOOLS_LOGLEVEL -gt 1 ]] && helm version;;
